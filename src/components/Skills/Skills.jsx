597bc92efe81b115f67953069c0d4dc58450d91b
soty <websoty@gmail.com>
2025-11-08 01:34:19 +0200

    feat: section skills content and styles

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,9 +1,79 @@
-import './Skills.css'
+import infoSkills from "../data/skills";
+import Title from "../Title/Title";
+import "./Skills.css";
 
 const Skills = () => {
+  const { skills, exp } = infoSkills;
   return (
-    <section>Skills</section>
-  )
-}
+    <section className="skills section" id="skills">
+      <div className="container flex-center">
+        <Title title={"Skills"} subtitle={"Skills"} />
+        <div className="inner-content">
+          <div className="skills-description">
+            <h3>Skills</h3>
+            <p>
+              Gaining practical experience in modern Front-End development, focusing on creating responsive and user-friendly web interfaces.
+            </p>
+          </div>
+          <div className="skills-info education-all">
+            <div className="education">
+              <h4 className="label">Skills</h4>
+              <ul className="bars">
+                {skills.map((item, index) => (
+                  <li className="bar" key={index}>
+                    <div className="info">
+                      <span>{item.skill}</span>
+                      <span>{item.grade}</span>
+                    </div>
+                    <div className="line javascript"></div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <div className="education work-exp">
+            <h3 className="work-exp-title">Work & Experience</h3>
+            <div className="skills-info">
+              {exp.map((item, index) => (
+                <div className="experience-card" key={index}>
+                  <strong><span>{item.work}</span></strong>
+                  <div className="hr"></div>
+                  <p>{item.profit}</p>
+                </div>
+              ))}
 
-export default Skills
+              {/* <div className="experience-card">
+                <div className="upper">
+                  <h3>Cr. Web Developer</h3>
+                  <h5>Full Time | InHouse</h5>
+                  <span>2019 - 2020</span>
+                </div>
+                <div className="hr"></div>
+                <h4 className="label">ib-themes ltd.</h4>
+                <p>
+                  I design and develop services for customers of all sizes,
+                  specializing in creating stylish, modern websites
+                </p>
+              </div>
+              <div className="experience-card">
+                <div className="upper">
+                  <h3>Jr. Web Developer</h3>
+                  <h5>Full Time | Remote</h5>
+                  <span>2018 - 2019</span>
+                </div>
+                <div className="hr"></div>
+                <h4 className="label">Creative Gigs.</h4>
+                <p>
+                  I design and develop services for customers of all sizes,
+                  specializing in creating stylish, modern websites
+                </p>
+              </div> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
---
+++ src/components/data/skills.js
@@ -0,0 +1,68 @@
+const skills = [
+  { skill: "Javascript", grade: "+" },
+  { skill: "React", grade: "+" },
+  { skill: "SCSS/SASS", grade: "+" },
+  { skill: "CSS", grade: "+" },
+  { skill: "GIT, GITHUB", grade: "+" },
+  { skill: "VITE", grade: "+" },
+  { skill: "API", grade: "+" },
+  { skill: "SPA", grade: "+" },
+  { skill: "ACCESSIBILITY", grade: "+" },
+  { skill: "CODE SPLITTING", grade: "+" },
+];
+
+const exp = [
+  {
+    work: "Developed landing pages and multi-page responsive websites improving UX and acheieving a consistent design across all devices",
+    profit:
+      "optimized development processes, reduce time to fix bugs and provide a stable, consistent user experience on all devices, which had a positive impact on engagement and conversion rates",
+  },
+  {
+    work: "Implemented interactive UI components (modals, sliders, forms, menus, tabs) using JavaScript, enhancing overall user engagement and interactivity.",
+    profit:
+      "optimized interface performance, improved element responsiveness, reduced UI-related errors, and increased user satisfaction and interaction speed.",
+  },
+  {
+    work: "Implemented dynamic content loading by fetching external HTML templates and REST API data using the Fetch API.",
+    profit:
+      "this allowed reusing markup across pages, avoiding code duplication, and improving site maintainability, optimization, and loading performance.",
+  },
+  {
+    work: "Optimized page performance by refining HTML/CSS structure, compressing media and fonts, and implementing lazy-loading, resulting in faster load times.",
+    profit:
+      "reduced overall page weight and improved load performance, delivering a faster and smoother user experience..",
+  },
+  {
+    work: "Used Git/GitHub for version control, organizing repositories and deploying projects on Vercel. and GitHub Pages, providing reliable delivery and real-time previews.",
+    profit:
+      "developed a transparent development process, simplified communication, quickly deployed and reduced the time for updating results",
+  },
+  {
+    work: "Gradually integrated React by creating reusable components, managing state and props, and handling events, preparing applications for scalable SPA architecture. ",
+    profit:
+      "allowed for increased scalability, simplified code maintenance, and faster development of new features.",
+  },
+  {
+    work: "Dynamic data integration & rendering",
+    profit:
+      "integrated data objects into reusable React components for dynamic list and block rendering, ensuring scalability and maintainability of the codebase.",
+  },
+  {
+    work: "State and event handling",
+    profit:
+      "implemented controlled components and event handlers to synchronize UI state and user interactions, enhancing responsiveness and reducing logic duplication.",
+  },
+  {
+    work: "Implemented SEO optimization and performance improvements",
+    profit:
+      "resulted in faster page load times and higher organic search visibility.",
+  },
+  {
+    work: "Data mapping and UI generation",
+    profit:
+      "leveraged array mapping and object iteration to generate dynamic UI elements from data sources, reducing hardcoded markup and streamlining content updates.",
+  },
+];
+
+const infoSkills = { skills, exp };
+export default infoSkills;
