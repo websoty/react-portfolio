befccbb99c22a0767c5429aa57880a7edcfd76f4
soty <websoty@gmail.com>
2025-11-09 14:54:45 +0200

    feat: section footer, fix: nav transition links

---
+++ src/components/data/footerData.js
@@ -0,0 +1,15 @@
+import { FaLinkedin, FaInstagram, FaGithub } from "react-icons/fa";
+
+export const infoLinks = [
+  { id: 1, href: "skills", label: "skills" },
+  { id: 2, href: "portfolio", label: "portfolio" },
+  { id: 3, href: "contact", label: "contact" },
+];
+
+
+export const followLinks = [
+  { id: 1, href: 'https://www.linkedin.com/in/stanislav-borodii/', icon: FaLinkedin },
+  { id: 2, href: 'https://www.instagram.com/naturesprout/', icon: FaInstagram },
+  { id: 3, href: 'https://github.com/websoty', icon: FaGithub },
+
+]
