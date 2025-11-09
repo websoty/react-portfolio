bd4c91b328ed8a5ac3fc21011556cf8d0a958146
soty <websoty@gmail.com>
2025-11-09 23:25:58 +0200

    feat: add ContactForm with standart web validation, EmailJS API sending + notification

--- src/components/Contacts/ContactForm.jsx
+++ src/components/Contacts/ContactForm.jsx
@@ -1,49 +1,64 @@
+import emailjs from "@emailjs/browser";
+import { useState } from "react";
 import { FaPaperPlane } from "react-icons/fa6";
-import { motion } from "motion/react";
-import { motionVariantY, motionVariant3} from "../../utils/animation";
-
 
 const ContactForm = () => {
+  const [statusMessage, setStatusMessage] = useState("");
+  const [statusType, setStatusType] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_jxpnzxx",
+        "template_3u295xj",
+        e.target,
+        "FhnEWzoiGASSArNjy"
+      )
+      .then(() => {
+        setStatusMessage("The letter has been sent ✅");
+        setStatusType("success");
+        e.target.reset();
+        setTimeout(() => setStatusMessage(""), 2000);
+      })
+      .catch(() => {
+        setStatusMessage("Error submitting ❌");
+        setStatusType("error");
+        setTimeout(() => setStatusMessage(""), 2000);
+      });
+  };
+
   return (
-    <form className="form contact__form">
-      <motion.div className="first__row"
-          initial="hidden"
-          whileInView="visible"
-          viewport={{once: false, amount: 0.5}}
-          custom={0}
-          variants={motionVariantY}
-      >
-        <input placeholder="Name" type="text" />
-      </motion.div>
-      <motion.div className="second__row"
-          initial="hidden"
-          whileInView="visible"
-          viewport={{once: false, amount: 0.5}}
-          custom={0}
-          variants={motionVariantY}
-      >
-        <input placeholder="Email" type="email" />
-        <input placeholder="Subject" type="text" />
-      </motion.div>
-      <motion.div className="third__row"
-          initial="hidden"
-          whileInView="visible"
-          viewport={{once: false, amount: 0.5}}
-          custom={0}
-          variants={motionVariantY}
-      >
-        <textarea placeholder="Message"></textarea>
-      </motion.div>
-      <motion.button className="contant__btn inner__info-link" type="submit"
-          initial="hidden"
-          whileInView="visible"
-          viewport={{once: false, amount: 0.5}}
-          custom={0}
-          variants={motionVariant3}
-      >
+    <form className="form contact__form" onSubmit={handleSubmit}>
+      <div className="first__row">
+        <input name="name" placeholder="Name" type="text" required />
+      </div>
+      <div className="second__row">
+        <input name="email" placeholder="Email" type="email" required />
+        <input name="subject" placeholder="Subject" type="text" required />
+      </div>
+      <div className="third__row">
+        <textarea name="message" placeholder="Message" required></textarea>
+      </div>
+      <button className="contant__btn inner__info-link" type="submit">
         Send Message
-        <FaPaperPlane/>
-      </motion.button>
+        <FaPaperPlane />
+      </button>
+
+      {statusMessage && (
+        <div
+          style={{
+            marginTop: "15px",
+            padding: "10px 15px",
+            borderRadius: "8px",
+            color: "#fff",
+            backgroundColor: statusType === "success" ? "green" : "red",
+            fontWeight: "500",
+          }}
+        >
+          {statusMessage}
+        </div>
+      )}
     </form>
   );
 };
