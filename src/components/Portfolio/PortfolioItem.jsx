afebb1cd5c4eb3e48b602520ed3c2f8d25dbe7b4
soty <websoty@gmail.com>
2025-11-09 18:38:13 +0200

    feat: animated section portfolio & touch

--- src/components/Portfolio/PortfolioItem.jsx
+++ src/components/Portfolio/PortfolioItem.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import PortfolioModal from "./PortfolioModal";
-
+import { motion } from "motion/react";
+import { motionVariant2 } from "../../utils/animation";
 
 const PortfolioItem = ({ item }) => {
 const [modalOpen, setModalOpen] = useState(false)
@@ -14,7 +15,13 @@ function modalClose () {
 }
 
   return (
-    <div className="portfolio__img-card">
+    <motion.div className="portfolio__img-card"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{once: false, amount: 0.5}}
+        custom={0}
+        variants={motionVariant2}
+    >
       <div className="img__card" onClick={openModal} >
         <div className="overlay"></div>
         <div className="info">
@@ -24,7 +31,7 @@ function modalClose () {
         <img src={item.imgSrc} alt={item.title} />
       </div>
       {openModal && <PortfolioModal item={item} modalOpen={modalOpen} modalClose={modalClose} />}
-    </div>
+    </motion.div>
   );
 };
 
--- src/components/touch/Touch.jsx
+++ src/components/touch/Touch.jsx
@@ -1,20 +1,33 @@
 import './Touch.css'
 import { FaPaperPlane } from "react-icons/fa";
+import { motion } from "motion/react";
+import { motionVariant, motionVariant3 } from "../../utils/animation";
 
 const Touch = () => {
   return (
     <div className="get__touch">
       <div className="container">
         <div className="contact__card">
-          <div className="title">
+          <motion.div className="title"
+              initial="hidden"
+              whileInView="visible"
+              viewport={{once: false, amount: 0.5}}
+              custom={0}
+              variants={motionVariant}
+          >
             <h4>Contact me</h4>
             <h3>to discuss</h3>
             <h2>opportunities</h2>
-          </div>
-          <a href="#" className="get__touch-link inner__info-link">
+          </motion.div>
+          <motion.a href="#" className="get__touch-link inner__info-link"
+              initial="hidden"
+              whileInView="visible"
+              viewport={{once: false, amount: 0.5}}
+              custom={1}
+              variants={motionVariant3}>
             Contact me
             <FaPaperPlane/>
-          </a>
+          </motion.a>
         </div>
       </div>
     </div>
